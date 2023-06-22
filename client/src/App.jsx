f9c8f4a01fe37d53386a28f7ca65d97733aba974
MrShifu01 <stander.christian.com>
2023-06-22 12:50:39 +0200

    basic ui setup, working on api's

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,20 @@
 import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import IndexPage from './pages/IndexPage'
+import LoginPage from './pages/LoginPage'
+import Layout from './Layout'
+import RegisterPage from './pages/RegisterPage'
 
 function App() {
 
-
   return (
-    <div>
-      Test
-    </div>
+    <Routes>
+      <Route path='/' element={<Layout/>}>
+        <Route index element={<IndexPage/>} />
+        <Route path='/login' element={<LoginPage/>} />
+        <Route path='/register' element={<RegisterPage/>} />
+      </Route>
+    </Routes>
   )
 }
 
